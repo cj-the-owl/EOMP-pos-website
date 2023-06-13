94b0e58aa7672748e1f61198f28e54586e9a048d
cj-the-owl <lodewykcarynjayn@gmail.com>
2023-06-13 16:33:23 +0200

    products not adding

--- script/products.js
+++ script/products.js
@@ -3,7 +3,7 @@ let products = [
         id: 1,
         name: "Spy x Family",
         image: "https://i.postimg.cc/cH18LK9b/Anya-Forger-Hoodie-1-removebg-preview.png",
-        price: "R300.00",
+        price: "300",
         quantity: 5,
         category: "Hoodies"
     },
@@ -11,7 +11,7 @@ let products = [
         id: 2,
         name: "My Hero Academia",
         image: "https://i.postimg.cc/Znbkgmq3/My-Hero-Academia-Hoodie-Beige-L-removebg-preview.png",
-        price: "R300.00",
+        price: "300",
         quantity: 5,
         category: "Hoodies"
     },
@@ -19,7 +19,7 @@ let products = [
         id: 3,
         name: "Hunter x Hunter",
         image: "https://i.postimg.cc/N0Fy8STj/Killua-and-Gon-Hoodie-White-S-removebg-preview.png",
-        price: "R300.00",
+        price: "300",
         quantity: 5,
         category: "Hoodies"
     },
@@ -27,7 +27,7 @@ let products = [
         id: 4,
         name: "Demon Slayer",
         image: "https://i.postimg.cc/hvLwCCxX/Roupinhas-da-shopee-removebg-preview.png",
-        price: "R300.300",
+        price: "300",
         quantity: 5,
         category: "Hoodies"
     },
@@ -35,7 +35,7 @@ let products = [
         id: 5,
         name: "Death Note",
         image: "https://i.postimg.cc/N0qvsr47/8-5-US-41-OFF-Death-Note-Anime-Sweatshirt-Manga-Sweatshirt-Women-Kawaii-Hoodies-Death-Note-H.png",
-        price: "R300.00",
+        price: "300",
         quantity: 5,
         category: "Hoodies"
     },
@@ -43,7 +43,7 @@ let products = [
         id: 6,
         name: "One Punch Man",
         image: "https://i.postimg.cc/2SNY32B1/One-Punch-Anime-Hoodie-Men-2022-Autumn-Fleece-Tracksuit-Male-Streetwear-Clothes-Fashion-Mens-Pullove.png",
-        price: "R300.00",
+        price: "300",
         quantity: 5,
         category: "Hoodies"
     },
@@ -51,7 +51,7 @@ let products = [
         id: 7,
         name: "Darling in the Franxx",
         image: "https://i.postimg.cc/Lscznx10/removebg-preview.png",
-        price: "R200.00",
+        price: "200",
         quantity: 5,
         category: "T-Shirts"
     },
@@ -59,7 +59,7 @@ let products = [
         id: 8,
         name: "Jujitsu Kaisen",
         image: "https://i.postimg.cc/9Q83577f/Lil-Gojo-Anime-Gojo-Satoru-Hot-sale-T-Shirt-Women-Fashion-Loose-Short-Sleeve-Cartoons-Style-Short-Sl.png",
-        price: "R200.00",
+        price: "200",
         quantity: 5,
         category: "T-Shirts"
     },
@@ -67,7 +67,7 @@ let products = [
         id: 9,
         name: "Naruto",
         image: "https://i.postimg.cc/662CdJ0b/download-1-removebg-preview.png",
-        price: "R200.00",
+        price: "200",
         quantity: 5,
         category: "T-Shirts"
     },
@@ -75,7 +75,7 @@ let products = [
         id: 10,
         name: "Attack on Titan",
         image: "https://i.postimg.cc/gkB91hR2/Intense-Power-Attack-On-Titan-T-Shirt-Haze-Blue-L-removebg-preview.png",
-        price: "R200.00",
+        price: "200",
         quantity: 5,
         category: "T-Shirts"
     },
@@ -83,7 +83,7 @@ let products = [
         id: 11,
         name: "One Piece",
         image: "https://i.postimg.cc/SKSLksH1/One-piece-removebg-preview.png",
-        price: "R200.00",
+        price: "200",
         quantity: 5,
         category: "T-Shirts"
     },
@@ -91,7 +91,7 @@ let products = [
         id: 12,
         name: "Spirited Away",
         image: "https://i.postimg.cc/wB8PVhyy/Spirited-Away-T-shirt-Anime-Cartoon-Anime-Harajuku-Graphic-Street-Style-T-shirt-For-Men-s-Women-s-Un.png",
-        price: "R200.00",
+        price: "200",
         quantity: 5,
         category: "T-Shirts"
     },
@@ -119,19 +119,19 @@ let products = [
 ];
 
 function displayProducts() {
-    let ourProducts = document.getElementById("products");
-    products.forEach((product) => {
+    const ourProducts = document.getElementById("products");
+    products.forEach((data) => {
         let productElement = document.createElement("div");
         productElement.innerHTML = `
-        <img src="${product.image}" alt="${product.name}" id="product-img">
-        <h3>${product.name}</h3>
-        <p>${product.price}</p>
-        <button onclick="addToCart(${product.id})" class=" btn add-btn">Add To Cart</button>`;
+        <img src="${data.image}" alt="${data.name}" id="product-img">
+        <h3>${data.name}</h3>
+        <p>${data.price}</p>
+        <button onclick="addToCart(${data.id})" class=" btn add-btn">Add To Cart</button>`;
         ourProducts.appendChild(productElement);
     });
 };
 
-let cart = JSON.parse(localStorage.getItem("Products")) || [];
+let cart = JSON.parse(localStorage.getItem("products")) || [];
 
 function addToCart(productId) {
     let product = products.find((product) => product.id === productId);
@@ -141,6 +141,39 @@ function addToCart(productId) {
         updateCart();
     }
 }
+
+function deleteFromCart(index) {
+    let deletedProduct = cart.splice(index, 1)[0];
+    deletedProduct.quantity++;
+    updateCart();
+}
+
+function updateCart() {
+    let cartContainer = document.getElementById("cart-body");
+    localStorage.setItem("products", JSON.stringify(cart));
+    cartContainer.innerHTML = "";
+    cart.forEach((product, index) => {
+        let cartProduct = document.createElement("div");
+        cartProduct.innerHTML = `
+        <span>${product.name}</span>
+        <span>${product.price}</span>
+        <input type="number" placeholder="1" min="1" width="50px" height="40px">
+        <p>Total ${product.price}</p>
+        <button onclick="deleteFromCart(${index})" class="delbtn">Delete</button>`;
+        cartContainer.appendChild(cartProduct);
+    });
+    calculateTotal();
+}
+
+function calculateTotal() {
+    let totalElement = document.getElementById("total");
+    let total = 0 
+    cart.forEach(item => {
+      total +=  eval(item.price)
+    })
+
+    totalElement.textContent = `${total}`;
+}    
 
 displayProducts();
 
